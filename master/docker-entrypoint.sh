915296594dec14865f4b02adb71b5dc984ffaba0
Alexandre Defelicibus <adefelicibus@gmail.com>
2022-03-24 07:13:21 -0300

    adaptacoes

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 export SLURM_CPUS_ON_NODE=$(cat /proc/cpuinfo | grep processor | wc -l)
-sudo sed -i "s/REPLACE_IT/CPUs=${SLURM_CPUS_ON_NODE}/g" /etc/slurm-llnl/slurm.conf
+sudo sed -i "s/CPUS/${SLURM_CPUS_ON_NODE}/g" /etc/slurm-llnl/slurm.conf
+
+m=$(lsmem --summary  | grep G | sed -e 's/[[:alpha:]]//g' -e 's/://g' -e 's/ //g')
+mem=$(printf "$((1024 * $m))")
+sudo sed -i "s/RAM/$mem/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
 sudo service slurmctld start
