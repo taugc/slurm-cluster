9183f8303666ef330e5a72e2c22e26a87648b096
Paulo Marcelo Rayner Oliveira <paulomarcelorayner@gmail.com>
2022-03-24 18:18:55 -0300

    Correção da captura de memória

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 export SLURM_CPUS_ON_NODE=$(cat /proc/cpuinfo | grep processor | wc -l)
 sudo sed -i "s/CPUS/${SLURM_CPUS_ON_NODE}/g" /etc/slurm-llnl/slurm.conf
 
-m=$(lsmem --summary  | grep G | sed -e 's/[[:alpha:]]//g' -e 's/://g' -e 's/ //g')
-mem=$(printf "$((1024 * $m))")
+m=$(grep MemTotal /proc/meminfo | sed -E 's/(.*) (.*) (.*)/\2/')
+mem=$(printf "$(($m / 1024))")
 sudo sed -i "s/RAM/$mem/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
