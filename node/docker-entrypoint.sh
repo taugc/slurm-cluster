7ab8f596ea03c4b56c587302e818f6d627fdf254
Paulo Marcelo Rayner Oliveira <paulomarcelorayner@gmail.com>
2022-03-31 17:38:57 -0300

    Alterações na captura da memória e start de serviços

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -3,12 +3,12 @@
 export SLURM_CPUS_ON_NODE=$(cat /proc/cpuinfo | grep processor | wc -l)
 sudo sed -i "s/CPUS/${SLURM_CPUS_ON_NODE}/g" /etc/slurm-llnl/slurm.conf
 
-m=$(grep MemTotal /proc/meminfo | sed -E 's/(.*) (.*) (.*)/\2/')
-mem=$(printf "$(($m / 1024))")
-sudo sed -i "s/RAM/$mem/g" /etc/slurm-llnl/slurm.conf
+# m=$(grep MemTotal /proc/meminfo | sed -E 's/(.*) (.*) (.*)/\2/')
+# mem=$(printf "$(($m / 1024))")
+# sudo sed -i "s/RAM/$mem/g" /etc/slurm-llnl/slurm.conf
 
-sudo systemctl start munge
-sudo systemctl start ssh 
+sudo service munge start 
+sudo service ssh start  
 sudo slurmd -N $SLURM_NODENAME
 
 tail -f /dev/null
